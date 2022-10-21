0f6e39b779b7ce7d0376725842a79c0d7fe92232
burraaook <57221787+burraaook@users.noreply.github.com>
2022-10-21 17:24:32 +0300

    create project files

---
+++ cmisbackend/src/main/java/com/los/cmisbackend/CmisbackendApplication.java
@@ -0,0 +1,13 @@
+package com.los.cmisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmisbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmisbackendApplication.class, args);
+	}
+
+}
