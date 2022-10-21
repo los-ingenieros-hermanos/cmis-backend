6765a6c27225dfcd894d6cd84c114c89104aa5b7
burraaook <57221787+burraaook@users.noreply.github.com>
2022-10-21 18:07:40 +0300

    update files

---
+++ cmis-backend/src/main/java/com/los/cmisbackend/CmisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.los.cmisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmisBackendApplication.class, args);
+	}
+
+}
