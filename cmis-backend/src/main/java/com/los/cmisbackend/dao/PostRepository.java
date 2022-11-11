8eaa8ca82c4fec124a9a7b6ed8e1ecf7afda518f
burraaook <57221787+burraaook@users.noreply.github.com>
2022-11-11 20:11:32 +0300

    create post repository

---
+++ cmis-backend/src/main/java/com/los/cmisbackend/dao/PostRepository.java
@@ -0,0 +1,8 @@
+package com.los.cmisbackend.dao;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="posts")
+public interface PostRepository {
+    
+}
