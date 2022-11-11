67f00fc9b3dfa82d4e49ef4cdb9fd6699cb9d955
burraaook <57221787+burraaook@users.noreply.github.com>
2022-11-11 20:11:16 +0300

    merge scripts

--- cmis-backend/mysql_scripts/create_db.sql
+++ cmis-backend/mysql_scripts/create_db.sql
@@ -13,9 +13,25 @@ CREATE TABLE `user` (
   `email` varchar(45) DEFAULT NULL,
   `password` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)	
-
 )ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `community`;
 
+CREATE TABLE `community` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `info` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `text` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)	
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
