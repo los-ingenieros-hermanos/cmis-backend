1cee9d6adcfcef8bf8e8d7529f14f3472e657252
AhmetKadir <ahmetkadir518@gmail.com>
2022-11-11 08:30:45 +0300

    Update community repo resource path

--- cmis-backend/src/main/java/com/los/cmisbackend/dao/CommunityRepository.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/dao/CommunityRepository.java
@@ -1,9 +1,11 @@
 package com.los.cmisbackend.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.los.cmisbackend.entity.Community;
 
+@RepositoryRestResource(path="communities")
 public interface CommunityRepository extends JpaRepository<Community, Integer> {
 
 }
