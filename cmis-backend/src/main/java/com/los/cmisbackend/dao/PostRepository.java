3a4b1bb7663d756c141420f9e64e24f269a57c7b
burraaook <57221787+burraaook@users.noreply.github.com>
2022-11-11 20:13:06 +0300

    Update PostRepository.java

--- cmis-backend/src/main/java/com/los/cmisbackend/dao/PostRepository.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/dao/PostRepository.java
@@ -1,8 +1,10 @@
 package com.los.cmisbackend.dao;
 
+import com.los.cmisbackend.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path="posts")
-public interface PostRepository {
-    
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
 }
