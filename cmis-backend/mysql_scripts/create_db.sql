4732deeea4ba187951c800144b6b5a31b8f212a6
burraaook <57221787+burraaook@users.noreply.github.com>
2022-11-11 19:21:48 +0300

    add user script

--- cmis-backend/mysql_scripts/create_db.sql
+++ cmis-backend/mysql_scripts/create_db.sql
@@ -4,3 +4,18 @@ CREATE SCHEMA `cmis-db`;
 
 use `cmis-db`;
 
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)	
+
+)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+
+
