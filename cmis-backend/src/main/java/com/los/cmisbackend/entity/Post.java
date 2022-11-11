263015f3065634642f512717d3d1ccc203df046c
burraaook <57221787+burraaook@users.noreply.github.com>
2022-11-11 19:42:27 +0300

    create post class

---
+++ cmis-backend/src/main/java/com/los/cmisbackend/entity/Post.java
@@ -0,0 +1,60 @@
+package com.los.cmisbackend.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="text")
+    private String text;
+
+    public Post() {
+
+    }
+
+    public Post(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
