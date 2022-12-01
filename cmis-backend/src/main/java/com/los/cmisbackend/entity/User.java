c7702da66d5256e34ed6a5ef0fa9fc885f4bc012
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-02 01:59:59 +0300

    Update User.java

--- cmis-backend/src/main/java/com/los/cmisbackend/entity/User.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/entity/User.java
@@ -1,70 +1,82 @@
 package com.los.cmisbackend.entity;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.los.cmisbackend.util.BCryptPasswordDeserializer;
-
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name="user")
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
 public class User {
-
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="id")
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column(name="first_name")
+    @NotBlank
+    @Size(max = 50)
     private String firstName;
 
-    @Column(name="last_name")
+    @Size(max = 50)
     private String lastName;
 
-    @Column(name="email")
+    @NotBlank
+    @Size(max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
     private String email;
 
-    @Size(min = 60, max = 60)
-    @Column(name="password", nullable = false, length = 60)
-    @JsonDeserialize(using = BCryptPasswordDeserializer.class )
+    @NotBlank
+    @Size(max = 120)
     private String password;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "bookmarkedpost_user",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"))
-    private List<Post> bookMarkedPosts;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
-    @ManyToMany(mappedBy = "followers")
-    private List<Community> followingCommunities;
-
-    public User () {
-
+    public User() {
     }
 
-    public User(String firstName, String lastName, String email, String password) {
+    public User(String firstName, String lastName, String username, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.username = email;
         this.email = email;
         this.password = password;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
@@ -73,53 +85,29 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-        System.out.println(password);
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getFirstName() {
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Post> getBookMarkedPosts() {
-        return bookMarkedPosts;
-    }
-
-    public void setBookMarkedPosts(List<Post> bookMarkedPosts) {
-        this.bookMarkedPosts = bookMarkedPosts;
-    }
-
-    
-    public List<Community> getFollowingCommunities() {
-        return followingCommunities;
-    }
-
-    public void setFollowingCommunities(List<Community> followingCommunities) {
-        this.followingCommunities = followingCommunities;
-    }
-
-    
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-
 }
