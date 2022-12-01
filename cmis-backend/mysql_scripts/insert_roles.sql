c0102e76efec830a4ce52360ad8d9fb6c62227e8
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-02 01:57:30 +0300

    Create insert_roles.sql

---
+++ cmis-backend/mysql_scripts/insert_roles.sql
@@ -0,0 +1,3 @@
+INSERT INTO `cmis-db`.`roles` (`id`, `name`) VALUES ('1', 'ROLE_STUDENT');
+INSERT INTO `cmis-db`.`roles` (`id`, `name`) VALUES ('2', 'ROLE_ADMIN');
+INSERT INTO `cmis-db`.`roles` (`id`, `name`) VALUES ('3', 'ROLE_COMMUNITY');
