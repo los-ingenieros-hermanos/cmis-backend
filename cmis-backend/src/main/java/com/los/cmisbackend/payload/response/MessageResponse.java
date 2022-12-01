cfefcc03579b657de382489f578ce31cdbabb132
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-02 02:00:08 +0300

    Create MessageResponse.java

---
+++ cmis-backend/src/main/java/com/los/cmisbackend/payload/response/MessageResponse.java
@@ -0,0 +1,17 @@
+package com.los.cmisbackend.payload.response;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
