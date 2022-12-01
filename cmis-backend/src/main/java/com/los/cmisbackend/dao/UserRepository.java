0954a73f18f336ffb69354f7831712a256be387d
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-02 01:59:16 +0300

    Update UserRepository.java

--- cmis-backend/src/main/java/com/los/cmisbackend/dao/UserRepository.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/dao/UserRepository.java
@@ -3,8 +3,18 @@ package com.los.cmisbackend.dao;
 import com.los.cmisbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(path="users")
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 }
