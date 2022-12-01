2615266172c3b85a42e74ff06ebd04ea83122768
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-02 02:00:12 +0300

    Create UserInfoResponse.java

---
+++ cmis-backend/src/main/java/com/los/cmisbackend/payload/response/UserInfoResponse.java
@@ -0,0 +1,63 @@
+package com.los.cmisbackend.payload.response;
+
+import java.util.List;
+
+public class UserInfoResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<String> roles;
+
+    public UserInfoResponse(Long id, String firstName, String lastName, String email, List<String> roles) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.email = username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
