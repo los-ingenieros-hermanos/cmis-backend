b40f01f0cb4bf7925322a8715e8b50fa46c0ee8b
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-03 00:01:40 +0300

    Update StudentRepository.java

--- cmis-backend/src/main/java/com/los/cmisbackend/dao/StudentRepository.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/dao/StudentRepository.java
@@ -4,8 +4,10 @@ import com.los.cmisbackend.entity.Post;
 import com.los.cmisbackend.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+    Student findStudentById(Long id);
+    List<Student> findStudentsByBookmarkedPostsId(Long id);
 }
