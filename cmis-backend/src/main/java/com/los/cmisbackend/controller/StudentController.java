391d4259788a0c45438f49410966e7d94dd63adc
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-03 00:01:19 +0300

    add follower, posts for student controller

--- cmis-backend/src/main/java/com/los/cmisbackend/controller/StudentController.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/controller/StudentController.java
@@ -1,17 +1,20 @@
 package com.los.cmisbackend.controller;
 
+import com.los.cmisbackend.dao.CommunityRepository;
 import com.los.cmisbackend.dao.StudentRepository;
+import com.los.cmisbackend.dao.UserRepository;
+import com.los.cmisbackend.entity.Community;
 import com.los.cmisbackend.entity.Student;
+import com.los.cmisbackend.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -22,7 +25,18 @@ public class StudentController {
     StudentRepository studentRepository;
 
     @Autowired
-    PasswordEncoder encoder;
+    UserRepository userRepository;
+
+    @Autowired
+    CommunityRepository communityRepository;
+
+    @GetMapping({ "/students/{id}", "/users/{id}/students" })
+    public ResponseEntity<Student> getStudentById(@PathVariable(value = "id") Long id) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found student with id = " + id));
+
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
 
     @GetMapping("/students")
     public ResponseEntity<List<Student>> getAllStudents() {
@@ -37,36 +51,32 @@ public class StudentController {
         return new ResponseEntity<>(students, HttpStatus.OK);
     }
 
-    @GetMapping("/students/{id}")
-    public ResponseEntity<Student> getStudentById(@PathVariable("id") Long id) {
-        Student student = studentRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + id));
+    @PostMapping("/users/{userId}/students")
+    public ResponseEntity<Student> createStudent(@PathVariable(value = "userId") Long userId,
+                                                         @RequestBody Student studentsRequest) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found User with id = " + userId));
 
-        return new ResponseEntity<>(student, HttpStatus.OK);
-    }
+        studentsRequest.setUser(user);
+        Student student = studentRepository.save(studentsRequest);
 
-    @PostMapping("/students")
-    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
-        Student _student = studentRepository.save(new Student(student.getFirstName(), student.getLastName(),
-                                                                student.getEmail(), encoder.encode(student.getPassword())));
-        return new ResponseEntity<>(_student, HttpStatus.CREATED);
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     @PutMapping("/students/{id}")
-    public ResponseEntity<Student> updateStudent(@PathVariable("id") long id, @RequestBody Student student) {
-        Student _student = studentRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + id));
+    public ResponseEntity<Student> updateStudent(@PathVariable("id") Long id,
+                                                         @RequestBody Student studentsRequest) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Id " + id + " not found"));
 
-        _student.setFirstName(student.getFirstName());
-        _student.setLastName(student.getLastName());
-        _student.setEmail(student.getEmail());
-        _student.setPassword(student.getPassword());
+        student.setBookMarkedPosts(studentsRequest.getBookMarkedPosts());
+        student.setFollowingCommunities(studentsRequest.getFollowingCommunities());
 
-        return new ResponseEntity<>(studentRepository.save(_student), HttpStatus.OK);
+        return new ResponseEntity<>(studentRepository.save(student), HttpStatus.OK);
     }
 
     @DeleteMapping("/students/{id}")
-    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable("id") long id) {
+    public ResponseEntity<HttpStatus> deleteStudent(@PathVariable("id") Long id) {
         studentRepository.deleteById(id);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
@@ -75,6 +85,48 @@ public class StudentController {
     @DeleteMapping("/students")
     public ResponseEntity<HttpStatus> deleteAllStudents() {
         studentRepository.deleteAll();
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/communities/{communityId}/followers")
+    public ResponseEntity<Set<Student>> getAllFollowersByCommunityId(@PathVariable(value = "communityId") Long communityId) {
+        Community community = communityRepository.findById(communityId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Not found Community with id = " + communityId));
+
+        Set<Student> students = community.getFollowers();
+        return new ResponseEntity<>(students, HttpStatus.OK);
+    }
+
+    @PostMapping("/communities/{communityId}/followers")
+    public ResponseEntity<Student> addFollowerToCommunity(@PathVariable(value = "communityId") Long communityId, @RequestBody Student followerRequest) {
+        Student follower = communityRepository.findById(communityId).map(community -> {
+            Long followerId = followerRequest.getId();
+
+            // follower is existed
+            if (followerId != null) {
+                Student _follower = studentRepository.findById(followerId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + followerId));
+                community.addFollower(_follower);
+                communityRepository.save(community);
+                return _follower;
+            }
+
+            throw new ResourceNotFoundException("Student does not exists.");
+        }).orElseThrow(() -> new ResourceNotFoundException("Not found Community with id = " + communityId));
+
+        return new ResponseEntity<>(follower, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/communities/{communityId}/followers/{followerId}")
+    public ResponseEntity<HttpStatus> deleteFollowerFromCommunity(@PathVariable(value = "communityId") Long communityId, @PathVariable(value = "followerId") Long followerId) {
+        Community community = communityRepository.findById(communityId)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Community with id = " + communityId));
+
+        Student follower = studentRepository.findById(followerId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + followerId));
+        community.removeFollower(follower);
+        communityRepository.save(community);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
