ac4bc4ebfbe2a03e8353e37907aeac28b6ef1ebe
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-02 04:36:13 +0300

    Update StudentRepository.java

--- cmis-backend/src/main/java/com/los/cmisbackend/dao/StudentRepository.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/dao/StudentRepository.java
@@ -1,11 +1,11 @@
 package com.los.cmisbackend.dao;
 
+import com.los.cmisbackend.entity.Post;
 import com.los.cmisbackend.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
 
-@RepositoryRestResource(path="students")
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
