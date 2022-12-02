05a9d335a3aa5267e2adb45be2a95e49fea1b068
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-03 00:01:29 +0300

    add user controller

---
+++ cmis-backend/src/main/java/com/los/cmisbackend/controller/UserController.java
@@ -0,0 +1,74 @@
+package com.los.cmisbackend.controller;
+
+import com.los.cmisbackend.dao.CommunityRepository;
+import com.los.cmisbackend.dao.StudentRepository;
+import com.los.cmisbackend.dao.UserRepository;
+import com.los.cmisbackend.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController()
+@RequestMapping("/api/cmis")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    CommunityRepository communityRepository;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = new ArrayList<User>();
+
+
+        userRepository.findAll().forEach(users::add);
+
+        if (users.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found User with id = " + id));
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") Long id) {
+        if (studentRepository.existsById(id)) {
+            studentRepository.deleteById(id);
+        }
+        else if (communityRepository.existsById((id))) {
+            communityRepository.deleteById(id);
+        }
+
+        userRepository.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/users")
+    public ResponseEntity<HttpStatus> deleteAllUsers() {
+        userRepository.deleteAll();
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
