508b291139cefc2d76eb03a1703056264fde995e
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-03 00:01:38 +0300

    Update PostRepository.java

--- cmis-backend/src/main/java/com/los/cmisbackend/dao/PostRepository.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/dao/PostRepository.java
@@ -1,10 +1,21 @@
 package com.los.cmisbackend.dao;
 
 import com.los.cmisbackend.entity.Post;
+import com.los.cmisbackend.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
 
-@RepositoryRestResource(path="posts")
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Post findPostById(Long id);
+    List<Post> findByCommunityId(Long id);
+
+    @Transactional
+    void deleteByCommunityId(Long id);
+
+    List<Post> findPostByStudentsId(Long id);
 }
