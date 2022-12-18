719f48c57ae2a28a3d7e0ae6e2a3892fc13b14f9
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-19 02:33:43 +0300

    initialize database with default tags

--- cmis-backend/src/main/java/com/los/cmisbackend/util/DbInit.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/util/DbInit.java
@@ -2,6 +2,9 @@ package com.los.cmisbackend.util;
 
 import javax.annotation.PostConstruct;
 
+import com.los.cmisbackend.dao.TagRepository;
+import com.los.cmisbackend.entity.ETag;
+import com.los.cmisbackend.entity.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,20 +16,45 @@ import com.los.cmisbackend.entity.Role;
 public class DbInit {
 	
 	@Autowired
-	private RoleRepository repository;
+	private RoleRepository roleRepository;
+
+	@Autowired
+	private TagRepository tagRepository;
 
 	@PostConstruct
 	private void init() {
 	
 		Long num = (long) 3;
 		
-		if(!(repository.findById(num).isPresent())){
+		if(!(roleRepository.findById(num).isPresent())){
 			Role role1 = new Role(ERole.ROLE_STUDENT);
 			Role role2 = new Role(ERole.ROLE_ADMIN);
 			Role role3 = new Role(ERole.ROLE_COMMUNITY);
-			repository.save(role1);
-			repository.save(role2);
-			repository.save(role3);
+			roleRepository.save(role1);
+			roleRepository.save(role2);
+			roleRepository.save(role3);
+		}
+
+		if(!(tagRepository.findById((long) 19).isPresent())) {
+			tagRepository.save(new Tag(ETag.TAG_ART));
+			tagRepository.save(new Tag(ETag.TAG_BIOLOGY));
+			tagRepository.save(new Tag(ETag.TAG_DRONE));
+			tagRepository.save(new Tag(ETag.TAG_CHEMISTRY));
+			tagRepository.save(new Tag(ETag.TAG_COMPUTER));
+			tagRepository.save(new Tag(ETag.TAG_ENGINEERING));
+			tagRepository.save(new Tag(ETag.TAG_ENTERTAINMENT));
+			tagRepository.save(new Tag(ETag.TAG_FOOD));
+			tagRepository.save(new Tag(ETag.TAG_GAMING));
+			tagRepository.save(new Tag(ETag.TAG_LITERATURE));
+			tagRepository.save(new Tag(ETag.TAG_MATH));
+			tagRepository.save(new Tag(ETag.TAG_MUSIC));
+			tagRepository.save(new Tag(ETag.TAG_PHILOSOPHY));
+			tagRepository.save(new Tag(ETag.TAG_PHYSICS));
+			tagRepository.save(new Tag(ETag.TAG_ROBOT));
+			tagRepository.save(new Tag(ETag.TAG_SCIENCE));
+			tagRepository.save(new Tag(ETag.TAG_SOCIAL));
+			tagRepository.save(new Tag(ETag.TAG_SPORT));
+			tagRepository.save(new Tag(ETag.TAG_TEAM));
 		}
 	}
 }
