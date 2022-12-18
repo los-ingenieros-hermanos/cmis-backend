123d71b64bfbee12adf7f8c8330b8e0ffda64868
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-19 02:31:10 +0300

    define enumerated tags

---
+++ cmis-backend/src/main/java/com/los/cmisbackend/entity/ETag.java
@@ -0,0 +1,23 @@
+package com.los.cmisbackend.entity;
+
+public enum ETag {
+    TAG_SCIENCE,
+    TAG_ENTERTAINMENT,
+    TAG_SOCIAL,
+    TAG_ROBOT,
+    TAG_FOOD,
+    TAG_SPORT,
+    TAG_TEAM,
+    TAG_ART,
+    TAG_COMPUTER,
+    TAG_BIOLOGY,
+    TAG_CHEMISTRY,
+    TAG_PHYSICS,
+    TAG_DRONE,
+    TAG_MUSIC,
+    TAG_GAMING,
+    TAG_MATH,
+    TAG_PHILOSOPHY,
+    TAG_LITERATURE,
+    TAG_ENGINEERING
+}
