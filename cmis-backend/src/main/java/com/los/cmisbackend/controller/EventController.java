6e219f4d832f8e6860d20131bee725d3a74152d7
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-23 06:01:27 +0300

    Create EventController.java

---
+++ cmis-backend/src/main/java/com/los/cmisbackend/controller/EventController.java
@@ -0,0 +1,117 @@
+package com.los.cmisbackend.controller;
+
+
+import com.los.cmisbackend.dao.EventRepository;
+import com.los.cmisbackend.dao.PostRepository;
+import com.los.cmisbackend.dao.StudentRepository;
+import com.los.cmisbackend.entity.Event;
+import com.los.cmisbackend.entity.Post;
+import com.los.cmisbackend.entity.Student;
+import com.los.cmisbackend.security.service.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/cmis")
+public class EventController {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/students/{id}/events")
+    public ResponseEntity<Set<Event>> getAllEventsByStudentId(@PathVariable(value = "id") Long id) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + id));
+
+        Set<Event> events = student.getEvents();
+        return new ResponseEntity<>(events, HttpStatus.OK);
+    }
+
+    @GetMapping("/students/{id}/events/{eventId}")
+    public ResponseEntity<Event> getEventByStudentId(@PathVariable(value = "id") Long id,
+                                                     @PathVariable(value = "eventId") Long eventId) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + id));
+        // get event
+        Event event = student.getEvents().stream()
+                .filter(e -> e.getId().equals(eventId))
+                .findFirst()
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Event with id = " + eventId));
+        // return event
+        return new ResponseEntity<>(event, HttpStatus.OK);
+    }
+
+    @PostMapping("/students/{id}/events")
+    public ResponseEntity<Event> addEventToStudent(@PathVariable(value = "id") Long id, @RequestBody Event eventRequest) {
+        // check authentication
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        if (!(userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))
+                | (userDetails.getId().equals(id))))
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+        Event event = studentRepository.findById(id).map(student -> {
+            Long eventId = eventRequest.getId();
+
+            // event is existed
+            if (eventId != null) {
+                Event _event = eventRepository.findById(eventId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Not found Event with id = " + eventId));
+                student.addEvent(_event);
+                studentRepository.save(student);
+                return _event;
+            }
+            throw new ResourceNotFoundException("Event does not exists.");
+        }).orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + id));
+
+        return new ResponseEntity<>(event, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/students/{id}/events/{eventId}")
+    public ResponseEntity<Event> deleteEventFromStudent(@PathVariable(value = "id") Long id,
+                                                        @PathVariable(value = "eventId") Long eventId) {
+        // check authentication
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        if (!(userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))
+                | (userDetails.getId().equals(id))))
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+        Event event = studentRepository.findById(id).map(student -> {
+            Event _event = student.getEvents().stream()
+                    .filter(e -> e.getId().equals(eventId))
+                    .findFirst()
+                    .orElseThrow(() -> new ResourceNotFoundException("Not found Event with id = " + eventId));
+            student.removeEvent(_event);
+            studentRepository.save(student);
+            return _event;
+        }).orElseThrow(() -> new ResourceNotFoundException("Not found Student with id = " + id));
+
+        return new ResponseEntity<>(event, HttpStatus.OK);
+    }
+
+    @GetMapping("/events/{id}/eventDetails")
+    public ResponseEntity<Post> getEventDetailByEventId(@PathVariable(value = "id") Long id) {
+        Event event = eventRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Event with id = " + id));
+        // find post by event id
+        Post post = postRepository.findPostByEventId(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found Post with event id = " + id));
+        // return post
+        return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+}
