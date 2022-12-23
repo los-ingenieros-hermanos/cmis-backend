fdf0d6defde941d5022a7cf705aa03bceac751b7
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-23 21:10:31 +0300

    update cross origin

--- cmis-backend/src/main/java/com/los/cmisbackend/controller/EventController.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/controller/EventController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
-@CrossOrigin(origins = {"*", "http://localhost:3000"}, maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/api/cmis")
 public class EventController {
--- cmis-backend/src/main/java/com/los/cmisbackend/controller/TagController.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/controller/TagController.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-@CrossOrigin(origins = {"*", "http://localhost:3000"}, maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("api/cmis")
 public class TagController {
