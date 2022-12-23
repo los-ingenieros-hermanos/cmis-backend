74131a8d4dca1b1470fbeda6c726bf2e91b7c902
burraaook <57221787+burraaook@users.noreply.github.com>
2022-12-23 21:20:56 +0300

    update cors

--- cmis-backend/src/main/java/com/los/cmisbackend/controller/StudentController.java
+++ cmis-backend/src/main/java/com/los/cmisbackend/controller/StudentController.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-@CrossOrigin(origins = "http://localhost:8070", maxAge = 3600)
+@CrossOrigin(origins = {"*", "http://localhost:3000", "http://127.0.0.1:3000"}, maxAge = 3600)
 @RestController
 @RequestMapping("api/cmis")
 public class StudentController {
