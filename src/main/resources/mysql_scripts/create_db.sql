55c0ea2dda70f0372211f7e5bd396251382f8234
AhmetKadir <ahmetkadir518@gmail.com>
2023-01-01 17:32:31 +0300

    Update for azure mysql database

--- src/main/resources/mysql_scripts/create_db.sql
+++ src/main/resources/mysql_scripts/create_db.sql
@@ -1,38 +1,4 @@
 DROP SCHEMA IF EXISTS `cmis-db`;
-
 CREATE SCHEMA `cmis-db`;
-
-use `cmis-db`;
-
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `password` varchar(70) DEFAULT NULL,
-  PRIMARY KEY (`id`)	
-)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `community`;
-
-CREATE TABLE `community` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `password` varchar(70) DEFAULT NULL,
-  `info` varchar(120) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `post`;
-
-CREATE TABLE `post` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) DEFAULT NULL,
-  `text` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`id`)	
-)ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
