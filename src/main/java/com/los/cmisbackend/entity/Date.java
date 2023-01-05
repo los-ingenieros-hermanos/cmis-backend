2910b95919da9d0acc59b9d4a4e26bff501746f3
burraaook <57221787+burraaook@users.noreply.github.com>
2023-01-05 22:21:58 +0300

    add minute hour to date

--- src/main/java/com/los/cmisbackend/entity/Date.java
+++ src/main/java/com/los/cmisbackend/entity/Date.java
@@ -1,6 +1,7 @@
 package com.los.cmisbackend.entity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name="date")
@@ -12,22 +13,30 @@ public class Date implements Comparable<Date> {
     private Long id;
 
     @Column(name="day")
-    private int year;
+    private Integer year = Integer.valueOf(2023);
 
     @Column(name="month")
-    private int month;
+    private Integer month = Integer.valueOf(1);
 
     @Column(name="year")
-    private int day;
+    private Integer day = Integer.valueOf(1);
+
+    @Column(name="hour")
+    private Integer hour = Integer.valueOf(0);
+
+    @Column(name="minute")
+    private Integer minute = Integer.valueOf(0);
 
     public Date() {
 
     }
 
-    public Date(int year, int month, int day) {
+    public Date(Integer year, Integer month, Integer day, Integer hour, Integer minute) {
         this.year = year;
         this.month = month;
         this.day = day;
+        this.hour = hour;
+        this.minute = minute;
     }
 
     public Long getId() {
@@ -38,31 +47,60 @@ public class Date implements Comparable<Date> {
         this.id = id;
     }
 
-    public int getYear() {
+    public Integer getYear() {
         return year;
     }
 
-    public void setYear(int year) {
+    public void setYear(Integer year) {
         this.year = year;
     }
 
-    public int getMonth() {
+    public Integer getMonth() {
         return month;
     }
 
-    public void setMonth(int month) {
+    public void setMonth(Integer month) {
         this.month = month;
     }
 
-    public int getDay() {
+    public Integer getDay() {
         return day;
     }
 
-    public void setDay(int day) {
+    public void setDay(Integer day) {
         this.day = day;
     }
 
+    public Integer getHour() {
+        return hour;
+    }
 
+    public void setHour(Integer hour) {
+        this.hour = hour;
+    }
+
+    public Integer getMinute() {
+        return minute;
+    }
+
+    public void setMinute(Integer minute) {
+        this.minute = minute;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Date date = (Date) o;
+        return Objects.equals(id, date.id) && Objects.equals(year, date.year) && Objects.equals(month, date.month) && Objects.equals(day, date.day) && Objects.equals(hour, date.hour) && Objects.equals(minute, date.minute);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, year, month, day, hour, minute);
+    }
+
+    // add compareTo method
     @Override
     public int compareTo(Date date) {
         if (this.year > date.year) {
@@ -80,39 +118,21 @@ public class Date implements Comparable<Date> {
                 } else if (this.day < date.day) {
                     return -1;
                 } else {
-                    return 0;
+                    if (this.hour > date.hour) {
+                        return 1;
+                    } else if (this.hour < date.hour) {
+                        return -1;
+                    } else {
+                        if (this.minute > date.minute) {
+                            return 1;
+                        } else if (this.minute < date.minute) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    }
                 }
             }
         }
-    }
-
-    @Override
-    public String toString() {
-        return "Date{" +
-                "id=" + id +
-                ", year=" + year +
-                ", month=" + month +
-                ", day=" + day +
-                '}';
-    }
-
-    // equals and hashcode methods are needed for comparing dates
-    // in the Event class
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Date date = (Date) o;
-        return year == date.year &&
-                month == date.month &&
-                day == date.day;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = year;
-        result = 31 * result + month;
-        result = 31 * result + day;
-        return result;
     }
 }
